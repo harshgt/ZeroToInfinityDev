f54ac80037f5802bdc5250a96e9f190635f84cf7
harshgt <harshgthakare@gmail.com>
2023-04-21 01:07:43 +0530

    2

--- force-app/main/default/lwc/zeroToInfyLWC/zeroToInfyLWC.js
+++ force-app/main/default/lwc/zeroToInfyLWC/zeroToInfyLWC.js
@@ -3,4 +3,18 @@ import { LightningElement } from 'lwc';
 export default class ZeroToInfyLWC extends LightningElement {
     salesforce = "salesforce Developer"
     name = "harshal"
+    role = "SFDC"
+
+    changeHandler(event){
+        this.role = event.target.value;
+    }
+    // obj = {
+
+    //     a: 12,
+    //     b: 13
+    // }
+    // nams = ["a","b","c","d"]
+
+
+    
 }
