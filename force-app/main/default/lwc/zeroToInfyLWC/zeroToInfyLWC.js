fa86f99dae4b3ac2d1c8131e11e2f02469676eb5
harshgt <harshgthakare@gmail.com>
2023-04-20 23:22:40 +0530

    Info2Two

--- force-app/main/default/lwc/zeroToInfyLWC/zeroToInfyLWC.js
+++ force-app/main/default/lwc/zeroToInfyLWC/zeroToInfyLWC.js
@@ -3,4 +3,6 @@ import { LightningElement } from 'lwc';
 export default class ZeroToInfyLWC extends LightningElement {
     salesforce = "salesforce Developer"
     name = "harshal"
+
+    InfoTwo2 = "InfoTwo2 not Default"
 }
